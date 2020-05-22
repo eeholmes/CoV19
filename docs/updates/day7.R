f17cd0fd36bc64b3a2b6ae14ba0246a583fcfb93
EEH <eli.holmes@noaa.gov>
2020-05-22 15:33:27 -0700

    update

---
+++ docs/updates/day7.R
@@ -0,0 +1,41 @@
+Update #7 (I think). Today the new cases in Lombardy are up from yesterday, but still well below 3 days ago (plot 1). In Hubei the new cases also went up and down while slowly trending downwards. Also for the 3rd day in a row the hospitalized numbers have not gone up. They are very high, but for many weeks they just kept increasing every day. The daily deaths have been steady (more or less) the last few days. The new deaths won't go down for awhile since deaths reflects what was happening with new cases 3-7 days (maybe longer) prior. At the current rate, the death toll will double at least :( but the exponential growth has stopped.  That could put the final toll in Lombardy near 10k but that is still far far below the 150k plus it could have been if they let the epidemic continue.
+
+In WA, we have more new cases than yesterday (so haven't leveled off yet) but our rate of increase condinues to slow. Yeah! We have four more days until the we are 12 days past when restaurants and bars were shutdown and recreation areas. Hopefully we'll see new cases stop increasing then, but our 'lockdown' is much softer than in Wuhan or Italy so we don't know yet. However our rate of increase is down to 1.13 (13% per day) which really gives us breathing room. Other states are at more like 30% per day and numbers just scream up with that rate of increase. That leaves hospitals no time to prepare.  
+
+I have been working on a page with all the code and plots. I have some extra plots on the page, like all the states (most of them) and then a page with just the states with the most cases. For those who want to work with the data, click on the link to the GitHub repository. https://eeholmes.github.io/CoV19/
+  
+For my California friends, here's a good article about how the Bay Area is also 'flattening' the curve. https://swell.life/article/rMYhMFSSqDXi/bayarea-is-flattening-the-curve   For my friends in other states, early actions to limit spread really do make a difference! Don't wait until you are overwhelmed. This doesn't mean lockdown for most communities. There are many things that can be done before that to slow the spread.
+
+-----------------------------------
+
+As usual Cuomo's press conference is very important to watch. Discussion on a national strategy of cooperation starts here: https://youtu.be/aTJPhVMn5D0?t=4024  NY is facing this first and there is much for all citizens to learn from how NY's governor is dealing with the epidemic. Cuomo speaks to the rest of the country at one point: "We are your future. Help New York to help yourself." By help, he wants the federal government to use Defense Procurement Act now. When NY is done with the ventilators, they will be moved to other cities (per Cuomo). NY needs 30,000 ventilators in 2 weeks and time is running out.
+
+"My mother is not expendable, your mother is not expendable." "You can have an intelligent refined public health strategy that allows people to go back to work." "What is this some modern Darwinian theory? You can't keep up you just fall to the wayside? God forbid."
+
+
+
+I keep hearing in the news that this is 'just' a disease that affects the elderly. I just don't get that sentiment. My back of the envelope calculation based on what we know about the disease, is that if we 'let it rip', the average HS of 3000 students would have 20-40 students who would lose at least one parent and 200-400 would lose at least one grandparent to CoV-19 (assuming all 4 grandparents are living). That feels like a big number to me. Ok, sure that's only 1.6% of students who lose at least one parent to CoV-19, but it still feels big to me. But adults die of other things! Well yes but the US death rate for CoV-19 for 45-65 years old is like 1-2% x probability of getting it (50-70% if we let it rip). So that's like a rate of 500 to 1000 per 100000 people. The overall death rate for people my age is 500 per 100000 for men and 320 per 100000 for women. If we let CoV-19 rip through the population that doubles the death rate of adults! How on earth is that not a lot?
+
+But hardly anyone has it! That's because it hasn't spread that much yet, but we now know that it is very infectious in when people gather in groups and that it would quickly infect much of the population. The costs of letting it lose seem really to me at least. Treatments and vaccines are being worked on by researchers all over the world. There have been promising treatments. It is possible to develop coronavirus vaccines (there are ones for domestic animals). A cure is almost certain to be available in 12 months and a vaccine not long after. Other countries have figured out how to keep working while keeping the virus as bay in their communities. We know that it is possible to knock down epidemics of CoV-19. Researchers are working on antibody tests so we know who is immune (and not a danger of spreading it). There are so many creative ways to restart the economy while also avoiding mass casualties. The whole world is working on this.
+
+---------------------------
+
+Here's my back of the envelope math:
+ 
+If you catch the virus 50-70%
+You are sick enouch to get tested ? 30% of US positives are 20-44. That's more than the frac of US populations that is 20-44.
+Fraction of positive who die 0.2% die age 10-39, 0.4% 40-49, 1.3 50-59, 3.6% 60-69, 8% 70-79, 14.8% 80+
+
+Let's say that half the people who get infected get sick enough to go get tested. We'll use a death rate (d) for those who have a confirmed infection (sick enough to go get a test) of 0.2% (dy) for HS  students and 2% (dp) for their 45-65 yr parents. Let N be the number of students in a HS or university.
+For the students, the number who die is N*0.7*.5*dy. Assuming each student has 2 parents, the number who lose a parent is N*(1-(1-0.7*.5*dp)^2). That icky bit is 1 - probability that neither parent dies.
+
+Let's assume 50% of people who catch it get sick enough to go to doctor (and get tested).
+Average HS of 3000 students would lose 2 students, 40 students would lose at least one parent.
+
+What about if 25% of the people who get infected get sick enough to go get tested.
+Average HS of 3000 students would lose 2 students, 20 students would lose at least one parent.
+
+
+
+
+

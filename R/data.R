b23d5354ef198071e638bf0ec5f614ecfc576779
EEH <eli.holmes@noaa.gov>
2020-06-17 15:42:19 -0700

    add states help

--- R/data.R
+++ R/data.R
@@ -7,7 +7,7 @@
 #'
 #' @format A data frame with 7 variables:
 #' \describe{
-#'   \item{date}{date. Data is for the previous day.}
+#'   \item{date}{date. Data are for the previous day.}
 #'   \item{region}{Province.}
 #'   \item{positive}{Cumulative positives aka totale_casi. Note the totale_positivi column in original data is current number of positives (total-recovered).}
 #'   \item{hospitalized}{Current number in the hospital. aka totale_ospedalizzati}
@@ -16,3 +16,23 @@
 #'   \item{ICU}{Current number in ICU. aka terapia_intensiva}
 #' }
 "italy"
+
+#' US states coronavirus data
+#'
+#' State data from Covid Tracking. The data cutoff is 4pm EST. 
+#' So some states are delayed by one day because they report after that time. Data are 
+#' downloaded from https://covidtracking.com/api/states/daily
+#' See `getdatastates.R` for processing of the data file.
+#'
+#' @format A data frame with 7 variables:
+#' \describe{
+#'   \item{date}{date. Data are for the previous day.}
+#'   \item{region}{State name}
+#'   \item{positive}{Cumulative positives.}
+#'   \item{negative}{Cumulative negatives (from testing).}
+#'   \item{hospitalized}{Current number in the hospital. aka totale_ospedalizzati}
+#'   \item{death}{Cumulative deaths}
+#'   \item{total tests}{Cumulative tests. These data are spotty and perhaps not very accurate.}
+#' }
+"states"
+
